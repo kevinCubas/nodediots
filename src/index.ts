24ccd0c658a09c1ce755dde05326e8949607e843
unknown <61201131+kevinCubas@users.noreply.github.com>
2022-07-15 16:43:01 -0300

    installed ts-node-dev and initialized server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Executei")
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res:Response, next:NextFunction) => {
+  res.status(200).send("Hello World2");
+})
+
+app.listen(3000, () => {
+  console.log('Executando na porta 3000')
+})
